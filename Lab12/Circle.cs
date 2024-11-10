942e1cddfa365bea4a2576bc0ee183ff995fa582
Павел Каплин <spike1321@yandex.ru>
2024-11-11 00:48:07 +0300

    Добавьте файлы проекта.

---
+++ Lab12/Circle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab12
+{
+    static class Circle
+    {
+        static public double GetSquare(double r)
+        {
+           
+            return Math.PI * Math.Pow(r, 2);
+        }
+        static public double GetLength(double r)
+        {
+            return 2 * Math.PI * r;
+        }
+        static public bool GetAffiliation(double r, double x0, double y0, double x, double y)
+        {
+            return Math.Sqrt((x0 - x) * (x0 - x) + (y0 - y) * (y0 - y)) <= r;
+        }
+    }
+}
---
+++ Lab12/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите координаты окружности:");
+            Console.Write("Введите радиус окружности: ");
+            double r = double.Parse(Console.ReadLine());
+            Console.Write("Введите координаты цента окружности x0 = ");
+            double x0 = double.Parse(Console.ReadLine());
+            Console.Write("Введите координаты цента окружности y0 = ");
+            double y0 = double.Parse(Console.ReadLine());
+            Console.Write("Введите координаты точки  x = ");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("Введите координаты точки  y = ");
+            double y = double.Parse(Console.ReadLine());
+
+            double square = Circle.GetSquare(r);
+            double length = Circle.GetLength(r);
+            bool affiliation = Circle.GetAffiliation(r, x0, y0,x,y);
+
+            Console.WriteLine("Площадь круга по заданному радиусу равна {0:f2}", square);
+            Console.WriteLine("Длину окружности по заданному радиусу равна {0:f2}", length);
+            Console.WriteLine(affiliation == true ? "точка принадлежит кругу": "точка не принадлежит кругу");
+            Console.ReadKey();
+        }
+    }
+}
